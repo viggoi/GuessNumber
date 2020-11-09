9128f2f304e4cbf5c48e7bf8d1089a1f0ec2b8ca
viggoi <viggo.ingmarsson@gmail.com>
2020-11-09 19:39:32 +0100

    Initial commit

---
+++ GuessNumber/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            while (true)
+            {
+                int randNum = random.Next(1, 101);
+                int guessedNum = 0;
+                int numGuesses = 0;
+                Console.WriteLine("Guess a number between 1-100");
+                while (guessedNum != randNum)
+                {
+                    Console.Write("Type your guess: ");
+                    if (int.TryParse(Console.ReadLine(), out guessedNum))
+                    {
+                        numGuesses += 1;
+                        if (guessedNum > randNum)
+                            Console.WriteLine("Your guess is too high");
+                        else if (guessedNum < randNum)
+                            Console.WriteLine("Your guess is too low");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not a number, try again");
+                    }
+                }
+
+                Console.WriteLine($"{randNum} was the correct number. You guessed it correctly in {numGuesses} tries");
+            }
+        }
+    }
+}
---
+++ GuessNumberGUI/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuessNumberGUI
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+        int randNum;
+        int numGuesses;
+        public Form1()
+        {
+            InitializeComponent();
+            NewNumber();
+        }
+
+        void NewNumber()
+        {
+            randNum = random.Next(1, 101);
+            numGuesses = 0;
+        }
+
+        void Check()
+        {
+            if (int.TryParse(textBox1.Text, out int guess))
+            {
+                numGuesses += 1;
+
+                if (guess == randNum)
+                {
+                    textBox2.Lines = new string[]
+                    {
+                        "You guessed correctly.",
+                        $"It took you {numGuesses} tries.",
+                        "A new number has been randomized.",
+                    };
+                    NewNumber();
+                }
+                else if (guess > randNum)
+                    textBox2.Text = $"You guessed too high";
+                else if (guess < randNum)
+                    textBox2.Text = $"You guessed too low";
+            }
+            else
+            {
+                textBox2.Text = "Not a number";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Check();
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Return)
+            {
+                e.SuppressKeyPress = true;
+                Check();
+            }
+        }
+    }
+}
